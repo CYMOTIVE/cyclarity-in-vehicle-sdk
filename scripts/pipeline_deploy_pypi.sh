51b0e087a426a3423db8edeb5eb9170b35f953ad
Gal Rahamim <gal.rahamim@cymotive.com>
2024-10-30 14:21:14 +0200

    CC-2559: update deploy script to handle versioning automatically

--- scripts/pipeline_deploy_pypi.sh
+++ scripts/pipeline_deploy_pypi.sh
@@ -5,6 +5,7 @@ export PYPI_TOKEN
 
 if [ "$BITBUCKET_BRANCH"  = "release" ]; then
     echo "The branch is release, releasing new version"
+    poetry version patch
 else
     echo "Releasing a prerelease version"
     poetry version prerelease
@@ -15,9 +16,28 @@ poetry build
 poetry config pypi-token.pypi $PYPI_TOKEN
 new_version=$(poetry version --short)
 
+if [ -f "cyclarity-in-vehicle-sdk.txt" ]; then    
+    echo "cyclarity-in-vehicle-sdk.txt exists, proceed."    
+else    
+    echo "cyclarity-in-vehicle-sdk.txt does not exist, creating now."    
+    touch cyclarity-in-vehicle-sdk.txt   
+    echo "cyclarity-in-vehicle-sdk.txt created."    
+fi
+
+echo "$new_version" > ./cyclarity-in-vehicle-sdk.txt
 echo "uploading $new_version"
 
 poetry publish
+
+git config --local user.name ci_user
+git config --local user.email bitbucket@ci
+
+git pull
+git add pyproject.toml
+git add cyclairty-sdk.txt
+git commit -m "[skip ci] ${new_version}"
+git tag ${new_version}
+git push origin ${new_version}
 
 echo "Upload finished, stopping script." 
 exit 0             
