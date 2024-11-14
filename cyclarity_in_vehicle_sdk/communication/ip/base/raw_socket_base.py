a20fc7b111b89df1042363814d681d9b09164c85
Eldad Sitbon <eldad.sitbon@cymotive.com>
2024-11-14 13:42:13 +0000

    CC-2557: implement a better base class for raw sockets with additional methods and option to send a sequence of packets

--- cyclarity_in_vehicle_sdk/communication/ip/base/raw_socket_base.py
+++ cyclarity_in_vehicle_sdk/communication/ip/base/raw_socket_base.py
@@ -1,6 +1,6 @@
 
 from abc import abstractmethod
-from typing import Callable, Optional
+from typing import Callable, Sequence
 from cyclarity_sdk.expert_builder.runnable.runnable import ParsableModel
 from py_pcapplusplus import Packet
 
@@ -20,39 +20,95 @@ class RawSocketCommunicatorBase(ParsableModel):
         raise NotImplementedError
     
     @abstractmethod
-    def send(self, packet: Packet) -> bool:
-        """send a packet of the raw socket
+    def is_open(self) -> bool:
+        """inform the state of the raw socket
+
+        Returns:
+            bool: True if the socket is open and ready for send/receive operations, False otherwise.
+        """
+        raise NotImplementedError
+    
+    @abstractmethod
+    def send(self, packet: Packet | Sequence[Packet]) -> bool:
+        """send a packet or a sequence of packets to the raw socket
 
         Args:
-            packet (Packet): packet o send
+            packet (Packet | Sequence[Packet]): packet/packets to send.
 
         Returns:
             bool: True if sent successfully, False otherwise
         """
         raise NotImplementedError
 
-    @abstractmethod
-    def send_receive_packet(self, packet: Packet, is_answer: Callable[[Packet], bool], timeout: float) -> Optional[Packet]:
-        """send packet and read an answer
+    def send_receive_packet(self, packet: Packet | Sequence[Packet] | None, is_answer: Callable[[Packet], bool], timeout: float) -> Packet | None:
+        """send packet or a sequence of packets and read an answer
+        The answer is one packet that satisfy the "is_answer" callback provided.
+
+        Note: This function uses the implementation of 'send_receive_packets', 
+        Optionally override this function to have a better implementation (stop after the first valid packet arrives).
 
         Args:
-            packet (Packet): packet to send_
-            is_answer (Callable[[Packet], bool]): callback that receives a packet and returns true if this packet is the answer to sent one
+            packet (Packet | Sequence[Packet] | None): the packet/packets to send. None to skip the sending operation.
+            is_answer (Callable[[Packet], bool]): callback that receives a packet and returns True if this packet is the answer to sent one
             timeout (int): timeout for the operation
 
         Returns:
-            Packet: the answer to the packet, None if not found
+            Packet | None: The first packet that satisfy the "is_answer" callback, None if not found.
         """
-        raise NotImplementedError
+        found_packets = self.send_receive_packets(packet, is_answer, timeout)
+        if found_packets:
+            return found_packets[0] # Get first valid answer
+        else:
+            return None
     
     @abstractmethod
-    def receive(self, timeout: float) -> Optional[Packet]:
+    def send_receive_packets(self, packet: Packet | Sequence[Packet] | None, is_answer: Callable[[Packet], bool], timeout: float) -> list[Packet]:
+        """send packet or a sequence of packets and read a multiple packets answer
+        The answer is a list of packets that satisfy the "is_answer" callback provided.
+
+        Args:
+            packet (Packet | Sequence[Packet] | None): the packet/packets to send. None to skip the sending operation.
+            is_answer (Callable[[Packet], bool]): callback that receives a packet and returns True if this packet is the answer to sent one
+            timeout (int): timeout for the operation
+
+        Returns:
+            list[Packet]: All packets received that satisfy the "is_answer" callback.
+        """ 
+        raise NotImplementedError
+
+    @abstractmethod
+    def receive(self, timeout: float) -> Packet | None:
         """read a single packet from the socket
 
         Args:
-            timeout (int): timeout for the operation
+            timeout (float): timeout in seconds for the operation, 0 for blocking receive.
 
         Returns:
-            Packet: the read packet
+            Packet | None: the read packet, None if timeout reached.
         """
         raise NotImplementedError
+
+    def receive_answer(self, is_answer: Callable[[Packet], bool], timeout: float) -> Packet | None:
+        """sniff communication and return a packet that satisfy the "is_answer" callback.
+
+        Args:
+            (Callable[[Packet], bool]): A callback that receives a packet and returns True if this packet is the answer looking for.
+            timeout (float): The duration of the sniffing to locate the answer packet.
+
+        Returns:
+            Packet | None: The first packet that satisfy the "is_answer" callback, None if not found.
+        """
+        return self.send_receive_packet(None, is_answer, timeout)
+    
+    def receive_answers(self, is_answer: Callable[[Packet], bool], timeout: float) -> Packet | None:
+        """Read a multiple packets and returns all packets that satisfy the "is_answer" callback provided.
+
+        Args:
+            is_answer (Callable[[Packet], bool]): A callback that receives a packet and returns True if this packet is the answer looking for.
+            timeout (int): The duration of the sniffing to locate the answer packets.
+
+        Returns:
+            list[Packet]: All packets received that satisfy the "is_answer" callback.
+        """ 
+        return self.send_receive_packets(None, is_answer, timeout)
+
