ba514e181162ffc0e9c0d95ca735e2a8d3514b3a
Gal Rahamim <gal.rahamim@cymotive.com>
2024-12-22 16:15:05 +0200

    CC-2632: add new type HexBytes

---
+++ cyclarity_in_vehicle_sdk/utils/custom_types/hexbytes.py
@@ -0,0 +1,17 @@
+from typing import Annotated, Any
+from pydantic import (
+    PlainSerializer,
+    PlainValidator,
+    errors,
+)
+
+def hex_bytes_validator(val: Any) -> bytes:
+    if isinstance(val, bytes):
+        return val
+    elif isinstance(val, bytearray):
+        return bytes(val)
+    elif isinstance(val, str):
+        return bytes.fromhex(val)
+    raise errors.BytesError()
+
+HexBytes = Annotated[bytes, PlainValidator(hex_bytes_validator), PlainSerializer(lambda v: v.hex())]
