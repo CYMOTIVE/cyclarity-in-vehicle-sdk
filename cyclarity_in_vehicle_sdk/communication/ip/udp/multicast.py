39bd156caf9f8ef021e5014ffff3779dfa4e29ab
aloosh <roi.aloosh@cymotive.com>
2025-03-09 15:38:39 +0200

    cc-2811: updated comments in UDP

--- cyclarity_in_vehicle_sdk/communication/ip/udp/multicast.py
+++ cyclarity_in_vehicle_sdk/communication/ip/udp/multicast.py
@@ -10,10 +10,15 @@ from cyclarity_in_vehicle_sdk.communication.base.communicator_base import Commun
 SOCK_DATA_RECV_AMOUNT = 4096
 
 # This class was just partially tested, and not in use by runnables ATM, do not use blindly
+## @brief A class used for multicast communication over IP networks.
+# @param interface_name The network interface name - needed in case of IPv6 multicast
+# @param _socket A socket object used for the communication
 class MulticastCommunicator(IpConnectionlessCommunicatorBase):
     interface_name: Optional[str] = Field(None, description="Network interface name - needed incase of IPv6 multicast")
     _socket: socket.socket = None
 
+    ## @brief Opens the socket and sets it up for multicast communication.
+    # @return A boolean indicating if the socket was successfully opened.
     def open(self) -> bool:
         if not self.destination_ip.is_multicast:
             raise RuntimeError(f"invalid multicast address provided: {str(self.destination_ip)}")
@@ -56,13 +61,23 @@ class MulticastCommunicator(IpConnectionlessCommunicatorBase):
         self._socket.setblocking(False)
         return True
     
+    ## @brief Closes the socket.
+    # @return A boolean indicating if the socket was successfully closed.
     def close(self) -> bool:
         self._socket.close()
         return True
     
+    ## @brief Sends data to the multicast group.
+    # @param data The data to be sent.
+    # @param timeout The timeout for the send operation.
+    # @return The number of bytes sent.
     def send(self, data: bytes, timeout: Optional[float] = None) -> int:
         return self._socket.sendto(data, (str(self.destination_ip), self.dport))
     
+    ## @brief Receives data from the multicast group.
+    # @param recv_timeout The timeout for the receive operation.
+    # @param size The size of the data to be received.
+    # @return The data received.
     def recv(self, recv_timeout: float = 0, size: int = SOCK_DATA_RECV_AMOUNT) -> bytes:
         recv_data: bytes = None
         if recv_timeout > 0:
@@ -73,9 +88,18 @@ class MulticastCommunicator(IpConnectionlessCommunicatorBase):
             pass
         return recv_data
     
+    ## @brief Sends data to a specific IP address and port.
+    # @param target_port The target port.
+    # @param target_ip The target IP address.
+    # @param data The data to be sent.
+    # @return The number of bytes sent.
     def send_to(self, target_port: int, target_ip: IPvAnyAddress, data: bytes) -> int:
         return self._socket.sendto(data, (target_ip.exploded, target_port))
-    
+
+    ## @brief Receives data from a specific IP address and port.
+    # @param size The size of the data to be received.
+    # @param recv_timeout The timeout for the receive operation.
+    # @return The data received and the sender's IP address.
     def receive_from(self, size: int = SOCK_DATA_RECV_AMOUNT, recv_timeout: int = 0) -> tuple[bytes, IPvAnyAddress]:
         recv_tuple: tuple[bytes, IPvAnyAddress] = (None, None)
         if recv_timeout > 0:
@@ -85,6 +109,8 @@ class MulticastCommunicator(IpConnectionlessCommunicatorBase):
         except BlockingIOError:
             pass
         return recv_tuple
-    
+
+    ## @brief Returns the type of the communicator.
+    # @return The type of the communicator.
     def get_type(self) -> CommunicatorType:
         return CommunicatorType.MULTICAST
