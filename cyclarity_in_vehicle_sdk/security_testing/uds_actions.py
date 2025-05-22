e39a0734be72dfd7125c425fa8b39e0686b2b149
Gal Rahamim <gal.rahamim@cymotive.com>
2025-05-22 16:08:01 +0300

    CC-2858: Introduce Session Control actions and outputs to the UDS framework, including validation logic for success and error scenarios, and refactor Read DID actions to use output_type for consistency.

--- cyclarity_in_vehicle_sdk/security_testing/uds_actions.py
+++ cyclarity_in_vehicle_sdk/security_testing/uds_actions.py
@@ -1,8 +1,10 @@
 from typing import Literal, Optional, Union
 from cyclarity_in_vehicle_sdk.protocol.uds.base.uds_utils_base import NegativeResponse, RdidDataTuple
 from cyclarity_in_vehicle_sdk.protocol.uds.impl.uds_utils import UdsUtils
+from cyclarity_in_vehicle_sdk.protocol.uds.models.uds_models import SESSION_INFO
 from cyclarity_in_vehicle_sdk.security_testing.models import BaseTestAction, BaseTestOutput, StepResult
 
+# ---------------- Read DID Action and Outputs ----------------
 
 class ReadDidOutputBase(BaseTestOutput):
     dids_data: list[RdidDataTuple] = []
@@ -12,7 +14,7 @@ class ReadDidOutputBase(BaseTestOutput):
         return StepResult(success=True)
 
 class ReadDidOutputExact(ReadDidOutputBase):
-    action_type: Literal['ReadDidOutputExact'] = 'ReadDidOutputExact'
+    output_type: Literal['ReadDidOutputExact'] = 'ReadDidOutputExact'
     def validate(self, step_output: "ReadDidOutputBase", prev_outputs: list["ReadDidOutputBase"] = []) -> StepResult:
         if self.dids_data != step_output.dids_data:
             return StepResult(success=False, fail_reason=f"Expected {self.dids_data} but got {step_output.dids_data}")
@@ -26,7 +28,7 @@ class ReadDidOutputExact(ReadDidOutputBase):
         return StepResult(success=True)
 
 class ReadDidOutputMaskMatch(ReadDidOutputBase):
-    action_type: Literal['ReadDidOutputMaskMatch'] = 'ReadDidOutputMaskMatch'
+    output_type: Literal['ReadDidOutputMaskMatch'] = 'ReadDidOutputMaskMatch'
     mask: int
     
     def validate(self, step_output: "ReadDidOutputBase", prev_outputs: list["ReadDidOutputBase"] = []) -> StepResult:
@@ -43,7 +45,7 @@ class ReadDidOutputMaskMatch(ReadDidOutputBase):
         return StepResult(success=True)
     
 class ReadDidOutputUnique(ReadDidOutputBase):
-    action_type: Literal['ReadDidOutputUnique'] = 'ReadDidOutputUnique'
+    output_type: Literal['ReadDidOutputUnique'] = 'ReadDidOutputUnique'
     def validate(self, step_output: "ReadDidOutputBase", prev_outputs: list["ReadDidOutputBase"] = []) -> StepResult:
         if step_output.error_code:
             return StepResult(success=False, fail_reason=f"Unexpected error code {hex(step_output.error_code)}")
@@ -59,6 +61,7 @@ class ReadDidOutputUnique(ReadDidOutputBase):
 
 
 class ReadDidAction(BaseTestAction):
+    action_type: Literal['ReadDidAction'] = 'ReadDidAction'
     dids: Union[int, list[int]]
     uds_utils: UdsUtils
     
@@ -71,3 +74,47 @@ class ReadDidAction(BaseTestAction):
             return ReadDidOutputBase(error_code=ex.code)
         finally:
             self.uds_utils.teardown()
+            
+# ---------------- Read DID Action and Outputs ----------------
+# ---------------- Session Control Action and Outputs ----------------
+
+class SessionControlOutputBase(BaseTestOutput):
+    error_code: Optional[int] = None
+    def validate(self, step_output: "SessionControlOutputBase", prev_outputs: list["SessionControlOutputBase"] = []) -> StepResult:
+        return StepResult(success=True)
+    
+class SessionControlOutputSuccess(SessionControlOutputBase):
+    output_type: Literal['SessionControlOutputSuccess'] = 'SessionControlOutputSuccess'
+    def validate(self, step_output: SessionControlOutputBase, prev_outputs: list[SessionControlOutputBase] = []) -> StepResult:
+        if step_output.error_code:
+            return StepResult(success=False, fail_reason=f"SessionControl service failed with error code {hex(self.error_code)}")
+        return StepResult(success=True)
+    
+class SessionControlOutputError(SessionControlOutputBase):
+    output_type: Literal['SessionControlOutputError'] = 'SessionControlOutputError'
+    def validate(self, step_output: SessionControlOutputBase, prev_outputs: list[SessionControlOutputBase] = []) -> StepResult:
+        if not step_output.error_code:
+            return StepResult(success=False, fail_reason="SessionControl service did not return an error code")
+        if self.error_code:
+            if self.error_code == step_output.error_code:
+                return StepResult(success=True)
+            else:
+                return StepResult(success=False, fail_reason=f"Expected {hex(self.error_code)} but got {hex(step_output.error_code)}")
+        return StepResult(success=True)
+
+class SessionControlAction(BaseTestAction):
+    action_type: Literal['SessionControlAction'] = 'SessionControlAction'
+    session_id: int
+    uds_utils: UdsUtils
+    
+    def execute(self) -> SessionControlOutputBase:
+        try:
+            self.uds_utils.setup()
+            self.uds_utils.session(session=self.session_id)
+            return SessionControlOutputBase()
+        except NegativeResponse as ex:
+            return SessionControlOutputBase(error_code=ex.code)
+        finally:
+            self.uds_utils.teardown()
+
+# ---------------- Session Control Action and Outputs ----------------
