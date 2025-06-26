33239cbc0b78b0a05c483672af1b59470816e039
eldadcool <eldad.sitbon@gmail.com>
2025-06-26 10:00:49 +0300

    add update related services (#11)
    * add update related services
    
    * fix review
    
    * fix transfer exit additional fields and fix req download format
    
    ---------
    
    Co-authored-by: Gal Rahamim <114643899+rahagal@users.noreply.github.com>
    Co-authored-by: Eldad Sitbon <eldad.sitbon@cymotive.com>

--- cyclarity_in_vehicle_sdk/protocol/uds/impl/uds_utils.py
+++ cyclarity_in_vehicle_sdk/protocol/uds/impl/uds_utils.py
@@ -2,6 +2,7 @@ import time
 from typing import Optional, Type, Union
 
 from pydantic import Field
+from udsoncan import MemoryLocation, DataFormatIdentifier
 from udsoncan.BaseService import BaseService
 from udsoncan.common.DidCodec import DidCodec
 from udsoncan.Request import Request
@@ -13,6 +14,9 @@ from udsoncan.services import (
     ReadDataByIdentifier,
     ReadDTCInformation,
     RoutineControl,
+    RequestDownload,
+    TransferData,
+    RequestTransferExit,
     SecurityAccess,
     TesterPresent,
     WriteDataByIdentifier,
@@ -298,7 +302,85 @@ class UdsUtils(UdsUtilsBase):
                                                                                    mode=SecurityAccess.Mode.SendKey)
         
         return interpreted_response.service_data.security_level_echo == security_algorithm.key_subfunction
-    
+
+    def request_download(self, address: int, memorysize: int, enc_comp: int = 0, address_format: int = 4,
+                         memorysize_format: int = 4, timeout: float = DEFAULT_UDS_OPERATION_TIMEOUT) -> int:
+        """Send a Request Download UDS message
+
+        Args:
+            timeout (float, optional): Timeout for the UDS operation in seconds. Defaults to DEFAULT_UDS_OPERATION_TIMEOUT.
+            address (int): Block ID or address of the relevant memory region to update.
+            memorysize (int): Size of the memory region to update.
+            enc_comp (int, optional): Encription and Compression info. Defaults to 0 (no encription and no compression).
+            address_format (int, optional): Length in bytes of the Address field. Defaults to 4.
+            memorysize_format (int, optional): Length in bytes of the Size field. Defaults to 4.
+
+        :raises RuntimeError: If failed to send the request
+        :raises ValueError: If parameters are out of range, missing or wrong type
+        :raises NoResponse: If no response was received
+        :raises InvalidResponse: with invalid reason, if invalid response has received
+        :raises NegativeResponse: with error code and code name, If negative response was received
+
+        Returns:
+            int: Maximum block length for following transfer data.
+        """
+        memory_location = MemoryLocation(
+            address=address,
+            memorysize=memorysize,
+            address_format=8*address_format,
+            memorysize_format=8*memorysize_format,
+        )
+
+        dfi = DataFormatIdentifier.from_byte(enc_comp)
+
+        request: Request = RequestDownload.make_request(memory_location, dfi)
+        response = self._send_and_read_response(request=request, timeout=timeout)
+        interpreted_response = RequestDownload.interpret_response(response=response)
+        return interpreted_response.service_data.max_length
+
+    def transfer_data(self, seq: int, data: bytes, timeout: float = DEFAULT_UDS_OPERATION_TIMEOUT) -> None:
+        """Transfer a block of data as part of Upload or Download session
+
+        Args:
+            timeout (float, optional): Timeout for the UDS operation in seconds. Defaults to DEFAULT_UDS_OPERATION_TIMEOUT.
+            seq (int): Sequence nuber of the current TransferData.
+            data (bytes): Data to be transfered.
+
+        :raises RuntimeError: If failed to send the request
+        :raises ValueError: If parameters are out of range, missing or wrong type
+        :raises NoResponse: If no response was received
+        :raises InvalidResponse: with invalid reason, if invalid response has received
+        :raises NegativeResponse: with error code and code name, If negative response was received
+        """
+
+        request: Request = TransferData.make_request(sequence_number=seq, data=data)
+        response = self._send_and_read_response(request=request, timeout=timeout)
+        interpreted_response = TransferData.interpret_response(response=response)
+        resp_seq = interpreted_response.service_data.sequence_number_echo
+        if resp_seq != seq:
+            raise InvalidResponse(f"Unexpected sequence number response {resp_seq}, expected {seq}.")
+
+    def transfer_exit(self, data: bytes | None = None, timeout: float = DEFAULT_UDS_OPERATION_TIMEOUT) -> bytes:
+        """Finish transfer session
+
+        Args:
+            data (bytes, optional): Additional optional data to send to the server
+            timeout (float, optional): Timeout for the UDS operation in seconds. Defaults to DEFAULT_UDS_OPERATION_TIMEOUT.
+
+        :raises RuntimeError: If failed to send the request
+        :raises ValueError: If parameters are out of range, missing or wrong type
+        :raises NoResponse: If no response was received
+        :raises InvalidResponse: with invalid reason, if invalid response has received
+        :raises NegativeResponse: with error code and code name, If negative response was received
+
+        Returns:
+            bytes: The parameter records received from the transfer exit response.
+        """
+        request: Request = RequestTransferExit.make_request(data=data)
+        response = self._send_and_read_response(request=request, timeout=timeout)
+        interpreted_response = RequestTransferExit.interpret_response(response=response)
+        return interpreted_response.service_data.parameter_records
+
     def read_dtc_information(self, 
                            subfunction: int,
                            status_mask: Optional[int] = None,
